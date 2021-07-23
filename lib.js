33b702299e2958a586543ec64632e5cfc18f05e1
Cloplan <87293903+Cloplan@users.noreply.github.com>
2021-07-23 07:09:34 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,5 +1,5 @@
 function load_home () {
-    fetch("https://generatinog.info/796" /*, options */)
+    fetch("https://generatinog.info/796/index.html" /*, options */)
     .then((response) => response.text())
     .then((html) => {
         document.querySelector("html").innerHTML = html;
